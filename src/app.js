aafec3abc84c4898bfb5ced693da04310622c224
Luis Felipe <l_felipelizeu@hotmail.com>
2021-10-20 15:35:20 -0300

    Created the connection to the database.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import connection from './database/database.js';
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Ok');
+app.get('/', async (req, res) => {
+    const users = (await connection.query(`SELECT * FROM users;`)).rows;
+
+    res.send(users);
 });
 
 app.listen(4000);
