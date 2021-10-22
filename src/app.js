e35beaaf49f7d8625a2eca93414dc62eeabbebe5
Luis Felipe <l_felipelizeu@hotmail.com>
2021-10-21 23:05:40 -0300

    Created a sign in function and /sign-in endpoint

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { signUpNewUser } from './controllers/users.js';
+import { signUpNewUser, signInUser } from './controllers/users.js';
 
 const app = express();
 app.use(express.json());
 
 app.post('/sign-up', signUpNewUser);
+
+app.post('/sign-in', signInUser);
 
 app.listen(4000);
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,6 +1,7 @@
 import connection from '../database/database.js';
 import { isSignUpDataValid } from '../validation/signUp.js';
 import bcrypt from 'bcrypt';
+import { v4 as uuid } from 'uuid';
 
 async function signUpNewUser (req, res) {
     if (!isSignUpDataValid(req.body)) return res.sendStatus(400);
@@ -20,6 +21,29 @@ async function signUpNewUser (req, res) {
     }
 }
 
+async function signInUser (req, res) {
+    const { email, password } = req.body;
+
+    try {
+        const result = await connection.query(`SELECT * FROM users WHERE email = $1 LIMIT 1;`, [email.trim()]);
+        const user = result.rows[0];
+
+        if (result.rowCount === 0) return res.sendStatus(404);
+        if (!bcrypt.compareSync(password, user.password)) return res.sendStatus(401);
+
+        const session = await connection.query(`SELECT * FROM sessions WHERE "userId" = $1 LIMIT 1;`, [user.id]);
+
+        if (session.rowCount !== 0) return res.status(200).send({ token: session.rows[0].token });
+
+        const token = uuid();
+        await connection.query(`INSERT INTO sessions ("userId", token) VALUES ($1, $2);`, [user.id, token]);
+        res.status(200).send({token});
+    } catch {
+        res.sendStatus(500);
+    }
+}
+
 export {
     signUpNewUser,
+    signInUser,
 }
