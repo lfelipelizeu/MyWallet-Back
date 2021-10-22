faee3b0b68fac2419af17886ca06d11f742407ac
Luis Felipe <l_felipelizeu@hotmail.com>
2021-10-21 21:21:47 -0300

    Fixed the sign up validation, now can't have whitespaces and must have a minimun length of 5.

--- src/validation/signUp.js
+++ src/validation/signUp.js
@@ -3,7 +3,7 @@ import joi from 'joi';
 const validateSignin = joi.object({
     name: joi.string().trim().min(3).required(),
     email: joi.string().trim().email().required(),
-    password: joi.string().pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/).required(),
+    password: joi.string().pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/).pattern(/\s/, { invert: true }).min(5).required(),
     repeatPassword: joi.ref('password')
 }).with('password', 'repeatPassword');
 
