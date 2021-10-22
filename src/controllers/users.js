3b3aa08b241de02b4994a346804ca17c94c08c2f
Luis Felipe <l_felipelizeu@hotmail.com>
2021-10-22 00:45:57 -0300

    Now the sign in request returns also the user's name

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -24,20 +24,25 @@ async function signUpNewUser (req, res) {
 async function signInUser (req, res) {
     const { email, password } = req.body;
 
+    if (!email || !password) return res.sendStatus(400);
+
     try {
         const result = await connection.query(`SELECT * FROM users WHERE email = $1 LIMIT 1;`, [email.trim()]);
         const user = result.rows[0];
 
-        if (result.rowCount === 0) return res.sendStatus(404);
+        if (!user) return res.sendStatus(404);
         if (!bcrypt.compareSync(password, user.password)) return res.sendStatus(401);
 
         const session = await connection.query(`SELECT * FROM sessions WHERE "userId" = $1 LIMIT 1;`, [user.id]);
 
-        if (session.rowCount !== 0) return res.status(200).send({ token: session.rows[0].token });
+        if (session.rowCount !== 0) return res.status(200).send({ name: user.name, token: session.rows[0].token });
 
         const token = uuid();
         await connection.query(`INSERT INTO sessions ("userId", token) VALUES ($1, $2);`, [user.id, token]);
-        res.status(200).send({token});
+        res.status(200).send({
+            name: user.name,
+            token
+        });
     } catch {
         res.sendStatus(500);
     }
