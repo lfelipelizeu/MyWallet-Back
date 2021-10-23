4de87823971d90808ce28d43e54bd4f74a9274c6
Luis Felipe <l_felipelizeu@hotmail.com>
2021-10-23 18:22:41 -0300

    Using cors at server to work properly in the browser

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import { signUpNewUser, signInUser } from './controllers/users.js';
 import { createNewTransaction, listUserTransactions } from './controllers/transactions.js';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.post('/sign-up', signUpNewUser);
@@ -12,5 +14,7 @@ app.post('/sign-in', signInUser);
 app.post('/transactions', createNewTransaction);
 
 app.get('/transactions', listUserTransactions);
+
+app.get('/health', (req, res) => res.status(200).send('Server is healty'));
 
 app.listen(4000);
