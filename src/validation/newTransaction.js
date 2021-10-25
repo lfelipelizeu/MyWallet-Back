1a30c676005efc6fd86b92dc3e2485d38e2f6e67
Luis Felipe <l_felipelizeu@hotmail.com>
2021-10-25 16:16:06 -0300

    Changed the validation to be easier to insert data

--- src/validation/newTransaction.js
+++ src/validation/newTransaction.js
@@ -2,7 +2,7 @@ import joi from 'joi';
 
 function isTransactionDataValid (object) {
     const transactionSchema = joi.object({
-        description: joi.string().min(1).max(50).required(),
+        description: joi.string().min(1).required(),
         value: joi.number().positive().precision(2).required(),
         type: joi.string().required()
     });
--- src/validation/signUp.js
+++ src/validation/signUp.js
@@ -2,9 +2,9 @@ import joi from 'joi';
 
 function isSignUpDataValid (object) {
     const signUpSchema = joi.object({
-        name: joi.string().trim().min(3).required(),
+        name: joi.string().trim().min(1).required(),
         email: joi.string().trim().email().required(),
-        password: joi.string().pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/).pattern(/\s/, { invert: true }).min(5).required(),
+        password: joi.string().required(),
         repeatPassword: joi.ref('password')
     }).with('password', 'repeatPassword');
 
