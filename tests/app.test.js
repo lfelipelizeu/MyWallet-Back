bd0bab8ecea42d359a028775d4b075315d0590d8
Luis Felipe <l_felipelizeu@hotmail.com>
2021-11-26 15:31:49 -0300

    Created a user factory and used it into the test now

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,41 +2,29 @@ import '../src/setup.js';
 import supertest from 'supertest';
 import connection from '../src/database/database.js';
 import app from '../src/app.js';
+import createUser from './factories/userFactory.js';
+import bcrypt from 'bcrypt';
 
 describe('POST /signup', () => {
-    afterEach(async () => {
-        await connection.query('DELETE FROM users WHERE name = \'Conta de teste\';');
+    const user = createUser();
+
+    beforeAll(async () => {
+        const hashPassword = bcrypt.hashSync(user.password, 10);
+        const insertedUser = await connection.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;', [user.name, user.email, hashPassword]);
+        user.id = insertedUser.rows[0].id;
+    });
+
+    afterAll(async () => {
+        await connection.query('DELETE FROM users;');
     });
 
     it('returns 400 for invalid body', async () => {
+        const testUser = createUser();
+
         const body = {
-            name: 'Conta de teste',
-            password: '12345',
-            repeatPassword: '12345',
-        };
-
-        const result = await supertest(app).post('/sign-up').send(body);
-        expect(result.status).toEqual(400);
-    });
-
-    it('returns 400 for invalid email', async () => {
-        const body = {
-            name: 'Conta de teste',
-            email: 'teste@email',
-            password: '12345',
-            repeatPassword: '12345',
-        };
-
-        const result = await supertest(app).post('/sign-up').send(body);
-        expect(result.status).toEqual(400);
-    });
-
-    it('returns 400 for not matching passwords', async () => {
-        const body = {
-            name: 'Conta de teste',
-            email: 'teste@email',
-            password: '12345',
-            repeatPassword: '123456789',
+            name: testUser.name,
+            password: testUser.password,
+            repeatPassword: testUser.password,
         };
 
         const result = await supertest(app).post('/sign-up').send(body);
@@ -44,13 +32,11 @@ describe('POST /signup', () => {
     });
 
     it('returns 409 for email already registered', async () => {
-        await connection.query('INSERT INTO users (name, email, password) VALUES (\'Conta de teste\', \'teste@email.com\', \'12345\')');
-
         const body = {
-            name: 'Conta de teste 2',
-            email: 'teste@email.com',
-            password: '12345',
-            repeatPassword: '12345',
+            name: user.name,
+            email: user.email,
+            password: user.password,
+            repeatPassword: user.repeatPassword,
         };
 
         const result = await supertest(app).post('/sign-up').send(body);
@@ -58,12 +44,7 @@ describe('POST /signup', () => {
     });
 
     it('returns 201 for valid data', async () => {
-        const body = {
-            name: 'Conta de teste',
-            email: 'teste@email.com',
-            password: '12345',
-            repeatPassword: '12345',
-        };
+        const body = createUser();
 
         const result = await supertest(app).post('/sign-up').send(body);
         expect(result.status).toEqual(201);
