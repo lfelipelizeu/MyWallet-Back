1fe69ca00636be8f9c2f6ec7d5cce61137a56d3d
Luis Felipe Silva Elizeu <87549176+lfelipelizeu@users.noreply.github.com>
2022-01-14 20:53:37 -0300

    Erase dev test

--- src/server.js
+++ src/server.js
@@ -5,5 +5,5 @@ const port = process.env.NODE_ENV === 'production' ? process.env.PORT : 4000;
 
 app.listen(port, () => {
     // eslint-disable-next-line no-console
-    console.log(`Server running on port ${port} on ${process.env.DB_DATABASE} database`);
+    console.log(`Server running on port ${port}`);
 });
