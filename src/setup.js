2027d9c7bc5bd8c77ca5f6bc0a2b22bd4be82f9e
Luis Felipe Silva Elizeu <87549176+lfelipelizeu@users.noreply.github.com>
2022-01-14 20:52:19 -0300

    Erase comments

--- src/setup.js
+++ src/setup.js
@@ -1,8 +1,6 @@
 import dotenv from 'dotenv';
 
 const envFile = `.env.${process.env.NODE_ENV}`;
-// if (process.env.NODE_ENV === 'test') envFile = '.env.test';
-// if (process.env.NODE_ENV === 'dev') envFile = '.env.dev';
 
 dotenv.config({
     path: envFile,
